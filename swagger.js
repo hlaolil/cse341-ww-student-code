fc5cd88a486781ea54d9364ac0e40039a1cc2db7
hlaolil <167699704+hlaolil@users.noreply.github.com>
2025-09-24 08:19:50 +0200

    Update swagger.js

--- swagger.js
+++ swagger.js
@@ -1,27 +1,12 @@
-const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
-
-const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Contacts API',
-      version: '1.0.0',
-      description: 'API for managing contacts (CSE341 Project Part 1)',
-    },
-    servers: [
-      {
-        url: 'http://localhost:3000',
-      },
-    ],
-  },
-  apis: ['./routes/*.js'], // scan route files for documentation
-};
-
-const swaggerSpec = swaggerJSDoc(options);
+const fs = require('fs');
+const path = require('path');
 
 function setupSwagger(app) {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  const swaggerDocument = JSON.parse(
+    fs.readFileSync(path.join(__dirname, 'swagger.json'), 'utf8')
+  );
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 }
 
 module.exports = setupSwagger;
